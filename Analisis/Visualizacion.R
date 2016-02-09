2398d6d73e5c10ed5368b53252bcd90210b800e2
Alberto <yoselerratil@gmail.com>
2016-02-08 19:25:31 -0600

    agrego llamadas a elementos graficos de graph_helpers.R

--- Analisis/Visualizacion.R
+++ Analisis/Visualizacion.R
@@ -1,7 +1,6 @@
-# éste script visualiza los datos 
+# visualizacion 
 setwd("~/Datos/Calentamiento/Analisis")
 library(ggplot2)
-#library(plotly)
 
 # cargado de datos
 emision_anio <- readRDS("../Datos/Emision_anio.Rds")
@@ -15,11 +14,10 @@ source("graph_helpers.R")
 ggplot(data = emision_anio, aes(x = anio, y = emision_anual)) +
     geom_point(size = 3, colour = "steelblue4") +
     geom_smooth(method = "lm", se = FALSE) +
-    eje_x_global +
-    scale_y_continuous(breaks = seq(from = 6e5, to = 8e5, by = 5e4),
-                       labels = as.character(seq(from = 600, to = 800, by = 50))) +
-    labs(x = "Año", y = "Emision anual [Gkg]") +
-    ggtitle("Emisión anual de CO2") +
+    eje_x_glob +
+    eje_y_glob +
+    labs(x = "Año", y = "Emisión anual [Gkg]") +
+    ggtitle("Emisión de CO2* en México") +
     estilo
 #dev.off()
 
@@ -27,12 +25,11 @@ ggplot(data = emision_anio, aes(x = anio, y = emision_anual)) +
 #png(filename = "../Imagenes/Emision_I.png", height = 675, width = 1200)
 ggplot(data = emision_I, aes(x = anio, y = emision_I, fill = categoria_1)) +
     geom_area(alpha = 0.85) + 
-    scale_y_continuous(breaks = seq(from = 0, to = 8e5, by = 1e5), expand = c(0.01, 0),
-                       labels = as.character(seq(from = 0, to = 800, by = 100))) +
+    eje_y_cat +
     eje_x_proporcional +
-    scale_fill_brewer(palette = "Accent", name = "Categoria", labels = etiquetas_I) +
-    labs(x = "Año", y = "Emisión anual de CO2 [Gkg]") + 
-    ggtitle("Emisión anual de CO2 por categoria") + 
+    scale_fill_brewer(palette = "Accent", name = "Categoría", labels = etiquetas_I) +
+    labs(x = "Año", y = "Emisión [Gkg]") + 
+    ggtitle("Emisión de CO2 por categoria") + 
     estilo
 #dev.off()
 
@@ -40,8 +37,7 @@ ggplot(data = emision_I, aes(x = anio, y = emision_I, fill = categoria_1)) +
 #png(filename = "../Imagenes/Emision_gas.png", height = 675, width = 1200)
 ggplot(data = emision_gas, aes(x = anio, y = evolucion_gas, fill = componente)) +
            geom_area(alpha = 0.85) + 
-    scale_y_continuous(breaks = seq(from = 0, to = 8e5, by = 1e5), expand = c(0.01, 0),
-                       labels = as.character(seq(from = 0, to = 800, by = 100))) +
+    eje_y_cat +
     eje_x_proporcional +
     scale_fill_brewer(palette = "Dark2", name = "Gas", labels = etiquetas_gas) +
     labs(x = "Año", y = "Emisión anual de CO2 [Gkg]") +
@@ -56,22 +52,20 @@ ggplot(data = emision_gas, aes(x = anio, y = evolucion_gas, fill = componente)) 
 ggplot(data = emision_I, aes(x = anio, y = emision_I, colour = categoria_1)) +
     geom_line(size = 1.3) + 
     geom_smooth(method = "lm", se = FALSE) +
-    scale_y_discrete(breaks = seq(from = 0, to = 5e5, by = 1e5),
-                       labels = as.character(seq(from = 0, to = 500, by = 100))) +
+    eje_y_individual +
     eje_x_proporcional +
     scale_color_brewer(palette = "Accent", name = "Categoria", labels = etiquetas_I) +
     labs(x = "Año", y = "Emisión anual de CO2 [Gkg]") + 
-    ggtitle("Emisión anual de Co2 por categoria") + 
+    ggtitle("Emisión anual de CO2 por categoria") + 
     estilo
 #dev.off()
 
 # por gases
 #png(filename = "../Imagenes/Emision_gas_lineal.png", height = 675, width = 1200)
 ggplot(data = emision_gas, aes(x = anio, y = evolucion_gas, colour = componente)) +
-    geom_line(size = 1.5, aes(fill = componente)) + 
+    geom_line(size = 1.3) + 
     geom_smooth(method = "lm", se = FALSE) +
-    scale_y_continuous(breaks = seq(from = 0, to = 5.5e5, by = 1e5),
-                       labels = as.character(seq(from = 0, to = 550, by = 100))) +
+    eje_y_individual +
     eje_x_proporcional +
     scale_color_brewer(palette = "Dark2", name = "Gas", labels = etiquetas_gas) +
     labs(x = "Año", y = "Emisión anual de CO2 [Gkg]") +
@@ -85,9 +79,9 @@ ggplot(data = emision_gas, aes(x = anio, y = evolucion_gas, colour = componente)
 #png(filename = "../Imagenes/Emision_I_proporcion.png", height = 675, width = 1200)
 ggplot(data = emision_I, aes(x = anio, y = emision_I, fill = categoria_1)) +
     geom_area(size = 1, position = "fill", aes(ymax = 1), alpha = 0.8) +
-    scale_y_continuous(breaks = seq(from = 0, to = 1, by = 0.1), expand = c(0,0)) +
+    eje_y_proporcional +
     eje_x_proporcional +
-    scale_fill_brewer(palette = "Accent", name = "Categoria", labels = etiquetas_I) +
+    scale_fill_brewer(palette = "Accent", name = "Categoría", labels = etiquetas_I) +
     labs(x = "Año", y = "Porcentaje") + 
     ggtitle("Participación anual en la emisión de CO2") + 
     estilo
@@ -97,25 +91,10 @@ ggplot(data = emision_I, aes(x = anio, y = emision_I, fill = categoria_1)) +
 #png(filename = "../Imagenes/Emision_gas_proporcion.png", height = 675, width = 1200)
 ggplot(data = emision_gas, aes(x = anio, y = evolucion_gas, fill = componente)) +
     geom_area(size = 1, position = "fill", aes(ymax = 1), alpha = 0.8) +
-    scale_y_continuous(breaks = seq(from = 0, to = 1, by = 0.1), expand = c(0,0)) +
+    eje_y_proporcional +
     eje_x_proporcional +
     scale_fill_brewer(palette = "Dark2", name = "Gas", labels = etiquetas_gas) +
     labs(x = "Año", y = "Porcentaje") +
     ggtitle("Composición de gases de efecto Invernadero") +
     estilo
 #dev.off()
-
-# plotly
-m <- ggplot(data = emision_I, aes(x = anio, y = emision_I, colour = categoria_1)) +
-    geom_line(size = 1.3) + 
-    geom_smooth(method = "lm", se = FALSE) +
-    facet_wrap(facets = ~ categoria_1, ncol = 4, scales = "fixed") +
-    scale_y_discrete(breaks = seq(from = 0, to = 5e5, by = 1e5),
-                     labels = as.character(seq(from = 0, to = 500, by = 100))) +
-    eje_x_proporcional +
-    scale_color_brewer(palette = "Accent", name = "Categoria", labels = etiquetas_I) +
-    labs(x = "Año", y = "Emisión anual de CO2 [Gkg]") + 
-    ggtitle("Emisión anual de Co2 por categoria") + 
-    estilo
-
-
--- Analisis/graph_helpers.R
+++ Analisis/graph_helpers.R
@@ -1,26 +1,32 @@
 ## elementos gráficos
 
 # estilo
-estilo <- theme(plot.title = element_text("Trebuchet MS", "bold", "#666666", size = 30)) +
-            theme(axis.title = element_text("Trebuchet MS", "bold","#666666", size = 20)) +
-            theme(axis.text.x = element_text("Trebuchet MS", size = 15, colour = "gray20"))
+estilo <- theme(plot.title = element_text("Trebuchet MS", "bold", "#666666", 30),
+                axis.title = element_text("Trebuchet MS", "bold","#666666", 20),
+                axis.text.x = element_text("Trebuchet MS", size = 15, colour = "gray15"))
 
-# eje
-eje_y_global_stack <- scale_y_continuous(breaks = seq(from = 6e5, to = 8e5, by = 5e4), limits = c(5.9e5, 8e5),
+# ejes
+eje_y_glob <-  scale_y_continuous(breaks = seq(6e5, 8e5, by = 5e4),
+                                  labels = c("600", "650", "700", "750", "800"))
+eje_y_cat <-  scale_y_continuous(breaks = seq(0, 8e5, by = 1e5), expand = c(0.01, 0),
+                    labels = as.character(seq(0, 800, by = 100)))
+
+eje_y_global_stack <- scale_y_continuous(breaks = seq(6e5, 8e5, by = 5e4), 
+                                         limits = c(5.9e5, 8e5), 
                                          labels = c("600", "650", "700", "750", "800"))
-eje_y_proporcional <- scale_y_continuous(breaks = seq(from = 0, to = 1, by = 0.1), expand = c(0,0))
+eje_y_proporcional <- scale_y_continuous(breaks = seq(0, 1, by = 0.1), expand = c(0,0))
 
-eje_y_individual <- scale_y_discrete(breaks = seq(from = 0, to = 5e5, by = 1e5),
-                                     labels = as.character(seq(from = 0, to = 500, by = 100)))
-
-eje_x_global <- scale_x_continuous(breaks = seq(1990, 2010, by = 5), expand = c(0, 0.5),
-                                   minor_breaks = seq(1990, 2010, 1)) 
+eje_y_individual <- scale_y_discrete(breaks = seq(0, 5e5, by = 1e5),
+                                     labels = as.character(seq(0, 500, by = 100)))
+# eje x
+eje_x_glob <- scale_x_continuous(breaks = seq(1990, 2010, by = 5), 
+                                   expand = c(0, 0.5), minor_breaks = seq(1990, 2010, 1)) 
 eje_x_proporcional <- scale_x_continuous(breaks = seq(1990, 2010, by = 5),
                                          minor_breaks = seq(1990, 2010, 1), expand = c(0,0))
     
 # guías
 etiquetas_I <- c("Agricultura", "Aviación y \nnavegación \ninternacional", "Desechos",
-                           "Quema de biomasa", "Energía", "Procesos industriales",
-                           "Uso de suelo, cambio\nde uso de suelo,\nsilvicultura")
+                 "Quema de biomasa", "Energía", "Procesos industriales",
+                 "Uso de suelo, cambio\nde uso de suelo,\nsilvicultura")
 
 etiquetas_gas <- c("CO2", "CH4", "N2O", "HCF's", "CF4", "C2F6", "SF6")
