acddea81e6f520165122563606b497dfd9596b99
Alberto <yoselerratil@gmail.com>
2016-02-10 14:46:39 -0600

    cambios menores

--- Analisis/Analisis.R
+++ Analisis/Analisis.R
@@ -1,4 +1,4 @@
-# analisare los datos, ya agrupados
+# analizare los datos, ya agrupados
 setwd("~/Datos/Calentamiento/Analisis")
 library(dplyr)
 library(ggplot2)
@@ -8,39 +8,37 @@ library(xts)
 datos <- readRDS("../Datos/Datos.Rds")
 
 ##  Preprocesado de datos
-
 # emision por año
 emision_anio <- datos %>%
                  group_by(anio) %>%
                  summarise(emision_anual = sum(emision))
-saveRDS(emision_anio, "../Datos/Emision_anio.Rds", compress = FALSE)
+#saveRDS(emision_anio, "../Datos/Emision_anio.Rds", compress = FALSE)
 # emision por jerarquia 1
 emision_I <- datos %>% group_by(anio, categoria_1) %>%
                   summarise(emision_I = sum(emision))
-saveRDS(emision_I, "../Datos/Emision_I.RDS", compress = FALSE)
+#saveRDS(emision_I, "../Datos/Emision_I.RDS", compress = FALSE)
 
 # emision por gas
 emision_gas <- datos %>%
                group_by(anio, componente) %>%
                summarise(evolucion_gas = sum(emision))
-saveRDS(emision_gas, "../Datos/Emision_componente.Rds", compress = FALSE)    
+#saveRDS(emision_gas, "../Datos/Emision_componente.Rds", compress = FALSE)    
 
 ## Datos para la interactividad
-ts_I <- spread(emision_I, key = categoria_1, value = emision_I)
+#ts_I <- spread(emision_I, key = categoria_1, value = emision_I)
 
-## Ahora voy mostrare la jerarquia II
-
+## mostrare la jerarquia II
 emision_II <- datos %>% 
               group_by(anio, categoria_1, categoria_2) %>%
               summarise(emision_anual = sum(emision))
+
 # separo las listas
 categorias_I <- as.character(unique(datos$categoria_1))
 emision_II_sep <- vector(mode = "list", length = 7)
-names(emision_II_sep) <- c("energía", "procesos_industriales",  "uso_suelo", "desechos", "bunkers", "quema_biomasa", "agricultura")
+names(emision_II_sep) <- c("energía", "procesos_industriales", "uso_suelo", 
+                           "desechos", "bunkers", "quema_biomasa", "agricultura")
 for (i in c(1:7)) {
     emision_II_sep[[i]] <- filter(emision_II, categoria_1 == categorias_I[i])
 }
 saveRDS(emision_II_sep, file = "../Datos/Emision_II_sep.Rds", compress = FALSE)
-
-
 
