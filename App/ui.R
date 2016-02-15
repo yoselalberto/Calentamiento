ba218dfb610ab77fbd8e706cb33ffc0a5e6f6210
Alberto <yoselerratil@gmail.com>
2016-02-15 00:18:43 -0600

    traduzco la interfaz a ingles

--- App/ui.R
+++ App/ui.R
@@ -10,32 +10,32 @@ shinyUI(fluidPage(
             p("Evolution of greenhouse effect gas emission in Mexico, per industry or per gas from 1990 to 2010"),
             
             conditionalPanel(condition = "input.pestania != 'anual'",
-                radioButtons(inputId = "opciones_comp", label = "Datos",
-                             c("Individuales" = "individuales",
-                                "Agrupados" = "agrupados"),
+                radioButtons(inputId = "opciones_comp", label = "Data",
+                             c("Individual" = "individuales",
+                                "Grouped" = "agrupados"),
                              selected = "individuales")
             ),
             conditionalPanel(condition = "input.pestania != 'anual' && input.opciones_comp == 'agrupados'",
-                radioButtons(inputId = "posicion_agrupados", label = "Tipo de gráfica",
+                radioButtons(inputId = "posicion_agrupados", label = "Kind of graph",
                              c("Area" = "area", 
-                               "Proporcional" = "proporcional"),
+                               "Proportional" = "proporcional"),
                              selected = "area")
             ),
             
             conditionalPanel(condition = "input.pestania == 'anual'",
-                             checkboxInput("ajuste", "Agregar un ajuste lineal", FALSE)
+                             checkboxInput("ajuste", "Add a linear adjust", FALSE)
             ),
             
             # rango de años a mostrar (zoom)
-            sliderInput("rango_anios", "Mostrar años",
+            sliderInput("rango_anios", "Range of years",
                         min = 1990, max = 2010, step = 1,
                         value = c(1990, 2010), ticks = FALSE),
-            p("*:", a("dioxido de carbono equivalente", href = "https://en.wikipedia.org/wiki/Carbon_dioxide_equivalent"))
+            p("*:", a("equivalent carbon dioxide", href = "https://en.wikipedia.org/wiki/Carbon_dioxide_equivalent"))
         ),
         
         mainPanel(
             tabsetPanel(id = "pestania",
-                tabPanel(title = "Anual", plotOutput("plot_anual"), value = "anual"),
+                tabPanel(title = "Annual", plotOutput("plot_anual"), value = "anual"),
                 tabPanel(title = "Sector", plotOutput("plot_sector", height = "500px"), value = "sector"),
                 tabPanel(title = "Gas", plotOutput("plot_gas", height = "500px"), value = "gas")
             )
